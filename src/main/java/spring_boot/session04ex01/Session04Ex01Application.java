5aa68e10a6a808688530dfd9fd4da6b55258374a
bangtrongtu <bangtu2005.com@gmail.com>
2026-05-19 07:50:23 +0700

    ex01

---
+++ src/main/java/spring_boot/session04ex01/Session04Ex01Application.java
@@ -0,0 +1,13 @@
+package spring_boot.session04ex01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session04Ex01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Session04Ex01Application.class, args);
+    }
+
+}
---
+++ src/test/java/spring_boot/session04ex01/Session04Ex01ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot.session04ex01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Session04Ex01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
